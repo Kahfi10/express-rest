5d0a2f73e614a6bd429fcbfaa69774a23f82a9ac
Ashabul Kahfi <105841108523@student.unismuh.ac.id>
2024-08-21 14:42:44 +0800

    chore: Update package.json with new dependencies

--- rest/index.js
+++ rest/index.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const { v4: uuidv4 } = require('uuid');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -9,22 +10,27 @@ app.set('view engine', 'ejs');
 
 const comments = [
     {
+        id: uuidv4(),
         username: 'Alice',
         comment: 'I love your website!'
     },
     {
+        id: uuidv4(),
         username: 'Bob',
         comment: 'Great job.'
     },
     {
+        id: uuidv4(),
         username: 'Charlie',
         comment: 'Nice work.'
     },
     {
+        id: uuidv4(),
         username: 'David',
         comment: 'I love it.'
     },
     {
+        id: uuidv4(),
         username: 'Eve',
         comment: 'Keep it up.'
     }
@@ -33,6 +39,22 @@ const comments = [
 app.get('/comments', (req, res) => {
     res.render('comments/index', { comments });
 });
+
+app.get('/comments/create', (req, res) => {
+    res.render('comments/create');
+});
+
+app.post('/comments', (req, res) => {
+    const { username, comment } = req.body;
+    comments.push({ username, comment, id: uuidv4()});
+    res.redirect('/comments');
+});
+
+app.get('/comments/:id', (req, res) => {
+    const { id } = req.params;
+    const comment = comments.find(c => c.id === id);
+    res.render('comments/show', { comment });
+ });
 
 app.get('/order', (req, res) => {
     res.send('Get Order API');
