b78109c9cbe9e441f490cec0e19815e263ee6f55
Ashabul Kahfi <105841108523@student.unismuh.ac.id>
2024-08-21 14:33:12 +0800

    chore: Add new dependencies and update package.json

--- rest/index.js
+++ rest/index.js
@@ -1,8 +1,38 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+const comments = [
+    {
+        username: 'Alice',
+        comment: 'I love your website!'
+    },
+    {
+        username: 'Bob',
+        comment: 'Great job.'
+    },
+    {
+        username: 'Charlie',
+        comment: 'Nice work.'
+    },
+    {
+        username: 'David',
+        comment: 'I love it.'
+    },
+    {
+        username: 'Eve',
+        comment: 'Keep it up.'
+    }
+];
+
+app.get('/comments', (req, res) => {
+    res.render('comments/index', { comments });
+});
 
 app.get('/order', (req, res) => {
     res.send('Get Order API');
